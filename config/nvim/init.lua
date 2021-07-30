dcc93c51a270a4dc0cccc62207a13db420494e80
Sam Skjonsberg <sams@allenai.org>
2021-07-29 21:49:32 -0700

    Telescope ignore settings.
    Ignore `node_modules` and Python cache files when using
    Telescope in nvim.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -145,6 +145,7 @@ require('telescope').setup {
         ['<C-d>'] = false,
       },
     },
+    file_ignore_patterns = { "node_modules", "__pycache__", "%.pyc" }
   },
 }
 -- Add leader shortcuts
