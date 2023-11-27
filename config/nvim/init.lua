f9447cb27e2170cfb156bc6406a22f46167a42d9
Sam Skjonsberg <sams@allenai.org>
2023-11-27 12:52:48 -0800

    Default to 2 spaces instead of 4

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -80,8 +80,8 @@ vim.cmd [[filetype plugin on]]
 
 -- Whitespace
 vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 -- Spelling
 vim.o.spelllang = 'en_us'
