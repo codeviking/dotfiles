df94a2a26742c116926b5661e786deff9fbebe01
Sam Skjonsberg <sam.skjonsberg@gmail.com>
2025-11-25 10:34:28 -0800

    Disable TODO highlighting

--- config/nvim/lua/plugins/code.lua
+++ config/nvim/lua/plugins/code.lua
@@ -5,4 +5,14 @@ return {
       inlay_hints = { enabled = false },
     },
   },
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      -- Show todo comments in the sign column but don't highlight the text
+      highlight = {
+        keyword = "",
+        after = "",
+      },
+    },
+  },
 }
